f8c8242e2b4f3bdaa61fa24399c48b742cb1503c
Alina Costa <alinacosta@Alinas-MBP-2.lan>
2022-09-05 14:50:50 -0400

    create department and role tables

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,16 @@
-INSERT INTO employees (first_name, last_name, role_id)
+INSERT INTO department (department_name)
+VALUES("Engineering"), ("Surveying"), ("Management");
+
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Engineer", 90000, 1),
+("Surveyer", 50000, 2),
+("Manager", 120000, 3);
+
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
-  ('Fluffy', 'Costa', 1),
-  ('Reina', 'Scav', 2),
-  ('Hobbs', 'Rososco', 3),
-  ('Donald', 'Duck', 4);
+  ('Fluffy', 'Costa', 1, 23),
+  ('Reina', 'Scav', 2, 24),
+  ('Hobbs', 'Rososco', 3, 25),
+  ('Donald', 'Duck', 4, 26);
